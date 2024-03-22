df5fdba1f5586cb51266ae5fe1c2829b7d440fa0
Omid Asadi <omiddzedd@gmail.com>
2024-03-22 02:42:43 -0700

    Created Contact Us Component, and improved responsiveness of footer, still unfinished

--- marios-pizza/src/App.tsx
+++ marios-pizza/src/App.tsx
@@ -1,26 +1,26 @@
-import { MenuView } from "./components/MenuView";
-import { Navbar } from "./components/NavBar";
-import { Categories } from "./components/Categories";
-import { Footer } from "./components/Footer";
+//import { MenuView } from './components/MenuView';
+import { Navbar } from './components/NavBar';
+//import { Categories } from './components/Categories';
+import { Footer } from './components/Footer';
+import { ContactUs } from './components/ContactUs';
 
 function App() {
-  return (
-    <div className="bg-primary h-screen">
-      <div>
-        <Navbar />
-      </div>
-      <div className="mt-6">
-        <Categories range="categories" />
-      </div>
-      <div className="bg-primary">
-        <MenuView range="Pizza" />
-        {/* <MenuView range="Soup" /> */}
-      </div>
-      <div className="bg-secondary text-white">
-        <Footer />
-      </div>
-    </div>
-  );
+	return (
+		<div className='bg-primary h-screen'>
+			<div>
+				<Navbar />
+			</div>
+			<div className='mt-6'>
+				{/* <Categories range='categories' /> */}
+			</div>
+			<div className='bg-primary'>
+				{/* <MenuView range='Pizza' /> */}
+				{/* <MenuView range="Soup" /> */}
+			</div>
+			<ContactUs />
+				<Footer />
+		</div>
+	);
 }
 
 export default App;
